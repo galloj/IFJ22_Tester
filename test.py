c3a1aa583e4dd1a08d635a70bde8037c6e9bc0a8
galloj <jiri.gallo@centrum.cz>
2022-10-26 15:59:57 +0200

    First interpreter test

--- test.py
+++ test.py
@@ -30,8 +30,17 @@ if len(sys.argv) < 2:
 	err("Please supply as first argument of test program path of your compiler")
 	exit()
 
+errors = 0
+successes = 0
 compiler_path = sys.argv[1]
-interpreter_path = ""
+interpreter_path = None
+
+if len(sys.argv) < 3:
+	err("Please supply as second argument of test program path of your interpreter")
+	note("Skipping interpreter tests")
+	errors += 1
+else:
+	interpreter_path = sys.argv[2]
 
 
 
@@ -44,6 +53,20 @@ def run_test(path, files):
 		expected_ret = [*map(int,open(path+"ret").read().split("|"))]
 		if ret_code not in expected_ret:
 			err(f"Wrong error code found {ret_code}, but expected {' or '.join(map(str,expected_ret))}")
+			is_ok = False
+	if "out" in files and interpreter_path is not None:
+		expected_out = open(path+"out").read()
+		# store current out to temp
+		temp_out = open("temp_out","w")
+		temp_out.write(out.decode("utf-8"))
+		temp_out.close()
+		proc = Popen([interpreter_path, "temp_out"], stdin=PIPE, stdout=PIPE, stderr=PIPE)
+		out_int,err_out_int = proc.communicate("")
+		ret_code_int = proc.returncode
+		if out_int.decode("utf-8") != expected_out:
+			err(f"Wrong output found {out_int.decode('utf-8')}, but expected {expected_out}")
+			print(err_out_int.decode('utf-8'))
+			print("INTERPRETER STDERR output:")
 			is_ok = False
 	if is_ok:
 		succ("TEST SUCCESS")
@@ -61,8 +84,15 @@ for root, dirs, files in os.walk("./tests"):
 		test(path[-1])
 		if not run_test(root + "/", files):
 			is_ok = False
+			errors += 1
+		else:
+			successes += 1
 	else:
 		print(Fore.YELLOW + "---"*(len(path)-1)+"> "+Fore.WHITE+path[-1])
+
+print(f"Tests finished with {Fore.RED}{errors} errors{Fore.WHITE} and {Fore.GREEN}{successes} successes{Fore.WHITE}")
+if errors == 0:
+	print(Fore.GREEN + "WELL DONE!" + Fore.WHITE)
 
 if not is_ok:
 	sys.exit(1)
